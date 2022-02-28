d93e7291c29572c84093c1dcb45cf232786c986f
Дроздов Даниил Константинович <d.drozdov.2021@stud.nstu.ru>
2022-02-28 13:34:29 +0700

    pt1 v2update

--- pt1_v2/pt1_v2.cpp
+++ pt1_v2/pt1_v2.cpp
@@ -5,62 +5,104 @@ const char endChar = '.';
 char target = NULL;
 
 struct list {
-    char elem;
-    list* next;
+   char elem;
+   list* next;
 
-    list(char _elem = NULL, list* _next = NULL) : elem(_elem), next(_next) {}
+   list(char _elem = NULL, list* _next = NULL) : elem(_elem), next(_next) {}
 
-    list* getStringEnd() {
-        list* c = this;
-        while (c->next != NULL && c->next->elem != separator && c->next->elem != endChar)
+   list* getStringEnd() {
+      list* c = this;
+      while (c->next != NULL && c->next->elem != separator && c->next->elem != endChar)
+         c = c->next;
+      return c;
+   }
+
+   void print() {
+      list* c = this;
+      while (c != NULL) {
+         printf("%c", c->elem);
+         c = c->next;
+      }
+   }
+
+   list* findElem(char t) {
+      list* c = this;
+      while (c != NULL && c->elem != t) c = c->next;
+      return c;
+   }
+
+   list* findPrev(list* h) {
+      list* c = h;
+      if (h == this) return NULL;
+      while (c != NULL && c->next != this) c = c->next;
+      return c;
+   }
+
+   void moveTo(list* t, list* prev) {
+      if (prev != NULL) prev->next = next;
+      next = t->next;
+      t->next = this;
+   }
+
+   void moveSequenceTo(list* sEnd, list* t, list* prev) {
+      if (this == sEnd) this->moveTo(t, prev);
+      else {
+         if (prev != NULL && sEnd->next != NULL) prev->next = sEnd->next;
+         sEnd->next = t->next;
+         t->next = this;
+      }
+   }
+
+    list* getSequenceEnd() {
+       if (this == NULL) return NULL;
+       list* c = this;
+       while (c->next != NULL && c->next->elem == elem) c = c->next;
+       return c;
+    }
+
+    bool isInSequence(list* sHead) {
+       if (this == NULL) return NULL;
+       list* c = sHead;
+       while (c->next != NULL && c->next->elem == elem && c != this) c = c->next;
+       return c == this ? true : false;
+    }
+
+    list* getSequenceHead(list* h) {
+       if (this == NULL) return NULL;
+       list* sHead = h->findElem(this->elem);
+       list* c = sHead;
+       while (sHead != NULL) {
+         while (c != NULL && c != this && sHead != this) {
             c = c->next;
-        return c;
-    }
-
-    void print() {
-        list* c = this;
-        while (c != NULL) { 
-            printf("%c", c->elem); 
-            c = c->next;
-        }
-    }
-
-    list* findElem(char t) {
-        list *c = this;
-        while (c != NULL && c->elem != t) c = c->next;
-        return c;
-    }
-
-    list* findPrev(list* h) {
-        list* c = h;
-        if (h == this) return NULL;
-        while (c != NULL && c->next != this) c = c->next;
-        return c;
-    }
-
-    void moveTo(list* t, list* prev) {
-        if (prev != NULL) prev->next = next;
-        next = t->next;
-        t->next = this;
+         }
+         if (c == this) return sHead;
+         sHead = c->findElem(this->elem);
+       }
+       return NULL;
     }
 
     list* moveToStringEnd(char t){
         if (next == NULL) return this;
         list* h = new list(NULL, this);
         list* first = h;
-        list* c = this->findElem(t);
-        list* last = c->getStringEnd();
+        list* sHead = this->findElem(t);
+        list* sEnd = sHead->getSequenceEnd();
+        list* lastHead = sHead->getStringEnd();
+        if (lastHead != NULL && lastHead->elem == t) {
+           lastHead = lastHead->getSequenceHead(first);
+        }
 
-        while (c != NULL) {
-            if(last != c){
-                last = c->getStringEnd();
-                c->moveTo(last, c->findPrev(h));
-                if(last->elem != t) last = c;
-                c = first->next->findElem(t);
+        while (sHead != NULL) {
+            if(lastHead != sHead && lastHead != sEnd){
+                lastHead = sHead->getStringEnd();
+                sHead->moveSequenceTo(sEnd, lastHead, sHead->findPrev(h));
+                if(lastHead != NULL && lastHead->elem != t) lastHead = sHead;
+                sHead = first->next->findElem(t);
             }
             else {
-                first = c->getStringEnd()->next;
-                c = first->findElem(t);
+                first = sHead->getStringEnd()->next;
+                sHead = first->findElem(t);
+                sEnd = sHead->getSequenceEnd();
             }
         }
         first = h->next;
