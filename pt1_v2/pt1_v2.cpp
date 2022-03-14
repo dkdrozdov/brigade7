df8ec60fc6b88dcb07a15395df5fb741fe85505c
Дроздов Даниил Константинович <d.drozdov.2021@stud.nstu.ru>
2022-03-14 13:29:09 +0700

    update

--- pt1_v2/pt1_v2.cpp
+++ pt1_v2/pt1_v2.cpp
@@ -4,78 +4,79 @@ const char separator = ' ';
 const char endChar = '.';
 char target = NULL;
 
-struct list {
+struct list
+{
    char elem;
-   list* next;
+   list *next;
 
-   list(char _elem = NULL, list* _next = NULL) : elem(_elem), next(_next) {}
+   list(char _elem = NULL, list *_next = NULL) : elem(_elem), next(_next) {}
 
-   list* getStringEnd() {
-      if (this == NULL) return NULL;
-      list* c = this;
-      while (c->next != NULL && c->next->elem != separator && c->next->elem != endChar)
-         c = c->next;
+   list *getStringEnd()      			//Возвращает конец строки.
+   {
+      if (!this) return NULL;
+      list *c = this;
+      for (; c->next && c->next->elem != separator && c->next->elem != endChar; c = c->next);
       return c;
    }
 
-   void print() {
-      list* c = this;
-      while (c != NULL) {
+   void print()              			//Печатает список в консоль.
+   {
+      for (list *c = this; c; c = c->next)
          printf("%c", c->elem);
-         c = c->next;
-      }
    }
 
-   list* findElem(char t) {
-      list* c = this;
-      while (c != NULL && c->elem != t) c = c->next;
+   list *findElem(char t)    			//Возвращает указатель на первое вхождение элемента t.
+   {
+      list *c = this;
+      for (; c && c->elem != t; c = c->next);
       return c;
    }
 
-   list* findPrev(list* h) {
-      list* c = h;
+   list *findPrev(list *h)   			//Возвращает предыдущий элемент. 
+   {                         			//h - первый элемент в списке.
+      list *c = h;
       if (h == this) return NULL;
-      while (c != NULL && c->next != this) c = c->next;
-      return c;
+      for (; c && c->next != this; c = c->next)
+         return c;
    }
 
-   void moveTo(list* t, list* prev) {
-      if (prev != NULL) prev->next = next;
-      next = t->next;
-      t->next = this;
-   }
-
-   void moveSequenceTo(list* sEnd, list* t, list* prev) {
-      if (this == sEnd) this->moveTo(t, prev);
-      else {
-         if (prev != NULL && sEnd->next != NULL) prev->next = sEnd->next;
+   //Вставляет последовательность элементов от данного до sEnd после t.
+   //prev - предыдущий элемент.
+   void moveSequenceTo(list *sEnd, list *t, list *prev)
+   {
+      if (this == sEnd)
+      {
+         if (prev) prev->next = next;
+         next = t->next;
+         t->next = this;
+      }
+      else
+      {
+         if (prev && sEnd->next) prev->next = sEnd->next;
          sEnd->next = t->next;
          t->next = this;
       }
    }
 
-   list* getSequenceEnd() {
-      if (this == NULL) return NULL;
-      list* c = this;
-      while (c->next != NULL && c->next->elem == elem) c = c->next;
+   list *getSequenceEnd()             //Возвращает конец последовательности одинаковых элементов.
+   {
+      if (!this) return NULL;
+      list *c = this;
+      for (; c->next && c->next->elem == elem; c = c->next);
       return c;
    }
 
-   list* moveToStringEnd(char t) {
-      if (next == NULL) return this;
-      list* h = new list(NULL, this);
-      list* first = h;
-      list* seqHead = this->findElem(t);
-      list* seqEnd = seqHead->getSequenceEnd();
-      list* strEnd = seqEnd->getStringEnd();
-
-      while (seqHead != NULL) {
-         if (seqEnd != strEnd) {
+   list *moveToStringEnd(char t)      //Перемещает все элементы t в конец строк, в которых находятся.
+   {
+      if (!next) return this;
+      list *h = new list(NULL, this), *first = h, *seqHead = this->findElem(t),
+         *seqEnd = seqHead->getSequenceEnd(), *strEnd = seqEnd->getStringEnd();
+      for (; seqHead;)
+      {
+         if (seqEnd != strEnd)
             seqHead->moveSequenceTo(seqEnd, strEnd, seqHead->findPrev(first));
-         }
-         else {
+         else
             first = strEnd->next;
-         }
          seqHead = first->findElem(t);
          seqEnd = seqHead->getSequenceEnd();
          strEnd = seqEnd->getStringEnd();
@@ -86,10 +87,11 @@ struct list {
    }
 };
 
-list* readFile() {
-   list* p = NULL;
-   list* current = NULL;
-   FILE* fp;
+list *readFile()                      //Возвращает указатель на новый список, составленный по содержимому файла.
+{
+   list *p = NULL;
+   list *current = NULL;
+   FILE *fp;
 
    fopen_s(&fp, "test.txt", "r");
    if (fp == 0)
@@ -98,28 +100,27 @@ list* readFile() {
    target = fgetc(fp);
    fgetc(fp);
 
-   char c = fgetc(fp);
-
-   while (c != EOF) {
-      if (current == NULL)
+   for (char c = fgetc(fp); c != EOF; c = fgetc(fp))
+   {
+      if (!current)
       {
          p = new list();
          current = p;
       }
-      else {
+      else
+      {
          current->next = new list();
          current = current->next;
       }
       current->elem = c;
-
-      c = fgetc(fp);
    }
    fclose(fp);
 
    return p;
 }
 
-int main() {
+int main()
+{
    readFile()->moveToStringEnd(target)->print();
    return 0;
 }
