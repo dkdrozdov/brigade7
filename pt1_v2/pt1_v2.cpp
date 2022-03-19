f0d6a84bf53e339953f4140fb138cad46c6376f6
dkdrozdov <dkdrozdov@gmail.com>
2022-03-19 18:13:42 +0700

    update

--- pt1_v2/pt1_v2.cpp
+++ pt1_v2/pt1_v2.cpp
@@ -11,7 +11,7 @@ struct list
 
    list(char _elem = NULL, list *_next = NULL) : elem(_elem), next(_next) {}
 
-   list *getStringEnd()      			//Возвращает конец строки.
+   list *getStringEnd()      			// Поиск конца строки
    {
       if (!this) return NULL;
       list *c = this;
@@ -19,38 +19,35 @@ struct list
       return c;
    }
 
-   void print()              			//Печатает список в консоль.
+   void print()
    {
       for (list *c = this; c; c = c->next)
          printf("%c", c->elem);
    }
 
-   list *findElem(char t)    			//Возвращает указатель на первое вхождение элемента t.
+   list *findElem(char s)    			// Поиск первого вхождения элемента s
    {
       list *c = this;
-      for (; c && c->elem != t; c = c->next);
+      for (; c && c->elem != s; c = c->next);
       return c;
    }
 
-   list *findPrev(list *h)   			//Возвращает предыдущий элемент. 
-   {                         			//h - первый элемент в списке.
+   list *findPrev(list *h)   			// Поиск предыдущего элемента
+   {                         			// h - первый элемент в списке
       list *c = h;
       if (h == this) return NULL;
       for (; c && c->next != this; c = c->next)
          return c;
    }
 
-   //Вставляет последовательность элементов от данного до sEnd после t.
-   //prev - предыдущий элемент.
    void moveSequenceTo(list *sEnd, list *t, list *prev)
-   {
-      if (this == sEnd)
-      {
+   {                                // Перемещение последовательности элементов
+      if (this == sEnd)             // от данного до sEnd на место после t
+      {                             // prev - предыдущий элемент
          if (prev) prev->next = next;
          next = t->next;
          t->next = this;
-      }
-      else
+      } else
       {
          if (prev && sEnd->next) prev->next = sEnd->next;
          sEnd->next = t->next;
@@ -58,7 +55,7 @@ struct list
       }
    }
 
-   list *getSequenceEnd()             //Возвращает конец последовательности одинаковых элементов.
+   list *getSequenceEnd()           // Поиск конца последовательности одинаковых элементов
    {
       if (!this) return NULL;
       list *c = this;
@@ -66,8 +63,8 @@ struct list
       return c;
    }
 
-   list *moveToStringEnd(char t)      //Перемещает все элементы t в конец строк, в которых находятся.
-   {
+   list *moveToStringEnd(char t)    // Перемещение всех элементов t в конец строк, 
+   {                                // в которых они находятся
       if (!next) return this;
       list *h = new list(NULL, this), *first = h, *seqHead = this->findElem(t),
          *seqEnd = seqHead->getSequenceEnd(), *strEnd = seqEnd->getStringEnd();
@@ -87,27 +84,23 @@ struct list
    }
 };
 
-list *readFile()                      //Возвращает указатель на новый список, составленный по содержимому файла.
+list *readFile()
 {
-   list *p = NULL;
-   list *current = NULL;
+   list *p = NULL, *current = NULL;
    FILE *fp;
 
    fopen_s(&fp, "test.txt", "r");
-   if (fp == 0)
-      return NULL;
+   if (!fp) return NULL;
 
    target = fgetc(fp);
    fgetc(fp);
-
    for (char c = fgetc(fp); c != EOF; c = fgetc(fp))
    {
       if (!current)
       {
          p = new list();
          current = p;
-      }
-      else
+      } else
       {
          current->next = new list();
          current = current->next;
